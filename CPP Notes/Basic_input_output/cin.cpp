218d21c42c6b56bfa3fec27c52ae53bc6fd8b3c6
Sundaram Pandey <sundarampandey004@gmail.com>
2024-04-10 13:38:08 +0530

    Basic Input and Output

---
+++ CPP Notes/Basic_input_output/cin.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std ;
+
+int main() 
+{
+    int age;
+    cout << "Enter your age:";
+    cin >> age; //The cin is a predefined object of istream class.
+    cout << "Your age is:" << age << endl;
+
+    return 0;
+}
