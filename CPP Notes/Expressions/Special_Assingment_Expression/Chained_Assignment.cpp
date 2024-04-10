4f892522d4e317c999d7a683ef7f9bc3fa840988
Sundaram Pandey <sundarampandey004@gmail.com>
2024-04-10 13:59:48 +0530

    Expressions in C++

---
+++ CPP Notes/Expressions/Special_Assingment_Expression/Chained_Assignment.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using  namespace std;
+int main() 
+{
+    int a;
+    int b;
+    a = b = 80; // Values can't be assinged during declaration
+    cout << "Values of 'a' and 'b' are :" << a << " " << b << endl;
+    return 0;
+}
